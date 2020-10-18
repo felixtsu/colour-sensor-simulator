24fe009989ae3d73ec3aac20f76263dc0dbe6f9b
felixtsu <humaeks@gmail.com>
2020-10-18 14:59:38 +0800

    完成了没有ui的逻辑和测试

--- main.ts
+++ main.ts
@@ -16,13 +16,18 @@ namespace coloursensorsim {
     let redLedOn : boolean
     let greenLedOn : boolean
     let blueLedOn : boolean
+    let environmentLight : number
 
     let speed = 1;
 
+    //%block
+    //%blockid=coloursensorsimsetspeed block="set simulator speed to %speed"
     export function setSpeed(speed:number) {
         speed = speed
     }
     
+    //%block
+    //%blockid=coloursensorsimgiveanswer block="give answer %colour"
     export function giveAnswer(colour:Colour) {
         console.log("receiving answer:" + colour + ", actual answer: " + currentBoxColour)
         if (colour == currentBoxColour) {
@@ -32,18 +37,26 @@ namespace coloursensorsim {
         }
     }
 
+    //%block
+    //%blockid=coloursensorsiminit block="init new test"
     export function initTest() {
         currentBoxColour = Math.pickRandom([Colour.Red, Colour.Green,
          Colour.Black, Colour.Blue, Colour.Yellow])
          redLedOn = false
          greenLedOn = false
          blueLedOn = false
+
+         environmentLight = randint(100,200) // mimic random environment light level;
     }
 
+    //%block
+    //%blockid=coloursensorsimpause block="pause %millis"
     export function pause(millis:number) {
         pause(millis * speed)
     }
 
+    //%block
+    //%blockid=coloursensorsimswitchled block="switch %ledColour LED to %on"
     export function switchLed(ledColour : LedColour, on:boolean) {
         switch(ledColour) {
             case LedColour.Red: 
@@ -58,8 +71,33 @@ namespace coloursensorsim {
         }
     }
 
+    //%block
+    //%blockid=coloursensorsimreadlightsensor block="read current light sensor value under LEDs status"
     export function readLightSensorValue() :number {
-        let result = randint(0, 1000)
+        // TODO: sim return value according to RGB on/off and box colour combinations
+
+        let result = environmentLight
+
+        if (redLedOn && currentBoxColour == Colour.Red) {
+            result += randint(100, 200) // led:box colour match
+        }
+
+        if (greenLedOn && currentBoxColour == Colour.Green) {
+            result += randint(100, 200) // led:box colour match
+        }
+
+        if (blueLedOn && currentBoxColour == Colour.Blue) {
+            result += randint(100, 200) // led:box colour match
+        }
+
+        if (blueLedOn && currentBoxColour == Colour.Yellow) {
+            result += randint(50, 100)
+        }
+
+        if (greenLedOn && currentBoxColour == Colour.Yellow) {
+            result += randint(50, 100)
+        }
+
         console.log("light sensor under [RGB]:[" + redLedOn + "|" + greenLedOn +  "|"  + blueLedOn + "]=" + result)
         return result
     }
--- test.ts
+++ test.ts
@@ -1,26 +1,34 @@
-// 在此处添加您的代码
+function testUnderBlue () {
+    coloursensorsim.switchLed(coloursensorsim.LedColour.Blue, true)
+    蓝灯照射下的光敏传感器的值 = coloursensorsim.readLightSensorValue() - 环境基线
+    coloursensorsim.switchLed(coloursensorsim.LedColour.Blue, false)
+}
+function testUnderRed () {
+    coloursensorsim.switchLed(coloursensorsim.LedColour.Red, true)
+    红灯照射下的光敏传感器数值 = coloursensorsim.readLightSensorValue()  - 环境基线
+    coloursensorsim.switchLed(coloursensorsim.LedColour.Red, false)
+}
+function testUnderGreen () {
+    coloursensorsim.switchLed(coloursensorsim.LedColour.Green, true)
+    绿灯照射下的光敏传感器的值 = coloursensorsim.readLightSensorValue() - 环境基线
+    coloursensorsim.switchLed(coloursensorsim.LedColour.Green, false)
+}
+let 绿灯照射下的光敏传感器的值 = 0
+let 红灯照射下的光敏传感器数值 = 0
+let 蓝灯照射下的光敏传感器的值 = 0
 coloursensorsim.initTest()
-
-let baselineValue = coloursensorsim.readLightSensorValue()
-
-coloursensorsim.switchLed(coloursensorsim.LedColour.Red, true)
-let redRelectionValue = coloursensorsim.readLightSensorValue()
-coloursensorsim.switchLed(coloursensorsim.LedColour.Red, false)
-
-coloursensorsim.switchLed(coloursensorsim.LedColour.Green, true)
-let greenRelectionValue = coloursensorsim.readLightSensorValue()
-coloursensorsim.switchLed(coloursensorsim.LedColour.Green, false)
-
-coloursensorsim.switchLed(coloursensorsim.LedColour.Blue, true)
-let blueRelectionValue = coloursensorsim.readLightSensorValue()
-coloursensorsim.switchLed(coloursensorsim.LedColour.Blue, false)
-
-let deltaRed = redRelectionValue - baselineValue
-let deltaGreen = greenRelectionValue - baselineValue
-let deltaBlue = blueRelectionValue - baselineValue
-
-if (deltaRed > 100 && deltaGreen > 100 && deltaBlue > 100) {
+let 环境基线 = coloursensorsim.readLightSensorValue()
+testUnderRed()
+testUnderGreen()
+testUnderBlue()
+if (蓝灯照射下的光敏传感器的值 < 50 && 绿灯照射下的光敏传感器的值 < 50 && 红灯照射下的光敏传感器数值 < 50) {
     coloursensorsim.giveAnswer(coloursensorsim.Colour.Black)
-} else {
+} else if (蓝灯照射下的光敏传感器的值 > 50 && 绿灯照射下的光敏传感器的值 > 50) {
+    coloursensorsim.giveAnswer(coloursensorsim.Colour.Yellow)
+} else if (红灯照射下的光敏传感器数值 > 100) {
     coloursensorsim.giveAnswer(coloursensorsim.Colour.Red)
+} else if (绿灯照射下的光敏传感器的值 > 100) {
+    coloursensorsim.giveAnswer(coloursensorsim.Colour.Green)
+} else if (蓝灯照射下的光敏传感器的值 > 100) {
+    coloursensorsim.giveAnswer(coloursensorsim.Colour.Blue)
 }
